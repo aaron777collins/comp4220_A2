38c665978aebee42969993424391f965501112f3
aaron777collins <aaron777collins@gmail.com>
2022-09-23 17:17:16 -0400

    init

---
+++ A2-CalculatorV2/src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+    public float multiply(float a, float b) {
+        return a*b;
+    }
+}
